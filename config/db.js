82f4c1de0b0301b15e3b35c199bd9c525b140da4
cesar <cesar.caju@academico.ifpb.edu.br>
2024-10-20 09:13:48 -0300

    removendo db

--- config/db.js
+++
@@ -1,7 +0,0 @@
-/*if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: } // conectar ao AWS
-} else{
-    module.exports = {mongoURI: "mongodb://localhost/blogapp" } // conecta localmente
-}*/
-
-module.exports = {mongoURI: "mongodb://localhost/blogapp" } // conecta localmente
